3f43e31d2c6bbf840ad0b0fa4f9c746e508140f0
sadat.malik <sadat.malik@me.com>
2021-08-02 15:13:24 +0100

    Completed Exercise_o4

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,26 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte b = 10;
+        System.out.println("byte b is: " + b);
+
+        short sh = 12;
+        System.out.println("short sh is: " + sh);
+
+        long ln = 1234L;
+        System.out.println("long ln is: " + ln);
+
+        float fp = 12.34f;
+        System.out.println("float fp is: " + fp);
+
+        double db = 1234.56789d;
+        System.out.println("double db is: " + db);
+
+        char c = 'a';
+        System.out.println("char c is: " + c);
+
+        boolean bool = false;
+        System.out.println("boolean bool is: " + bool);
 
     }
 
