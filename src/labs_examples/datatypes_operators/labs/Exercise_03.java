d8f3c5fd61cf508327b402648c3725c3bcf1e064
sadat.malik <sadat.malik@me.com>
2021-08-04 10:56:46 +0100

    Completed Exercise_03

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,20 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int i = 2 + 3;
+        System.out.println("2 + 3 = " + i);
+
+        i = 5 - 2;
+        System.out.println("5 - 2 = " + i);
+
+        i = 3 * 4;
+        System.out.println("3 * 4 = " + i);
+
+        i = 20 / 5;
+        System.out.println("20 / 5 = " + i);
+
+        i = 17 % 6;
+        System.out.println("17 % 6 = " + i);
 
     }
 
