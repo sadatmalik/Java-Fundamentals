60d3c8821d85f39e5ec60c66c92fa6f9a2b34961
sadat.malik <sadat.malik@me.com>
2021-08-04 12:35:53 +0100

    Completed datatypes_operators Exercise_05

--- src/labs_examples/datatypes_operators/labs/Exercise_05.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_05.java
@@ -14,10 +14,35 @@ class LogicalOperators {
         boolean a = true;
         boolean b = false;
         if (a | b){
-            System.out.println("a or b is true");
+            System.out.println("#1 a or b is true");
         }
 
         // write your code below
+        // example of short-circuit OR
+        if (a || b) {
+            System.out.println("#2 a or b is true");
+        }
+
+        // AND
+        boolean c = true;
+        if (a & c) {
+            System.out.println("#3 a and c are true");
+        }
+
+        // short-circuit AND
+        if (a && c) {
+            System.out.println("#4 a and c are true");
+        }
+
+        // XOR
+        if (a ^ b) {
+            System.out.println("#5 a or b is true but not both");
+        }
+
+        // NOT
+        if (!b) {
+            System.out.println("#6 b is not true");
+        }
 
     }
 
