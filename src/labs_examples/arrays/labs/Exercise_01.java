c70f72521f5487d82b993d418f133d30445bb7b7
sadat.malik <sadat.malik@me.com>
2021-08-06 13:42:17 +0100

    Completed arrays Exercise_01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -12,4 +14,20 @@ package labs_examples.arrays.labs;
 
 public class Exercise_01 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter number " + (i+1) + " : ");
+            nums[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        System.out.println("Sum = " + sum);
+        System.out.println("Avg = " + sum/nums.length);
+    }
 }
