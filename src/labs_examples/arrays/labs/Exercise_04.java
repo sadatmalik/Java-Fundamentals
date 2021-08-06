a239931cdadab7ba862d6a95b0da6c41fb6f853f
sadat.malik <sadat.malik@me.com>
2021-08-06 13:56:53 +0100

    Completed arrays Exercise_04

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -10,4 +10,22 @@ package labs_examples.arrays.labs;
 
 public class Exercise_04 {
 
+    public static void main(String[] args) {
+
+        int[][] multi = {
+                {2, 3, 4},
+                {5, 6},
+                {7, 8, 9, 10},
+                {11, 12, 13},
+                {15}
+        };
+
+        for (int[] inner : multi) {
+            for (int i : inner) {
+                System.out.print(i + " | ");
+            }
+            System.out.println();
+        }
+    }
+
 }
