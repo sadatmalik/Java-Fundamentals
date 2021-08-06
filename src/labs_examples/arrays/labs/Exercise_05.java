b2369eb08ac002f7e793d5dbc90f5511ab7a6956
sadat.malik <sadat.malik@me.com>
2021-08-06 14:00:34 +0100

    Completed arrays Exercise_05

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -10,4 +10,12 @@ package labs_examples.arrays.labs;
 
 public class Exercise_05 {
 
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = array.length-1; i >= 0; i = i - 2) {
+            System.out.println(array[i]);
+        }
+    }
+
 }
