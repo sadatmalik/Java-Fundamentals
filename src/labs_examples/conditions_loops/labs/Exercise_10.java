a07086288718ad5bf7f8fe4b9d50cbaeccf658a1
sadat.malik <sadat.malik@me.com>
2021-08-06 11:33:54 +0100

    Completed conditions_loops Exercise_10

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -8,4 +8,17 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_10 {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 10) {
+            i++;
+            if (i == 3 || i == 7) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
 }
