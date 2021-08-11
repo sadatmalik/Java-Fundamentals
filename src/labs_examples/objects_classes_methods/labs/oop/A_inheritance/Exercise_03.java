492e4bf90c42bc25661473f26f2de5718998bbe3
sadat.malik <sadat.malik@me.com>
2021-08-11 16:56:19 +0100

    Completed oopA Exercise_03

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -3,7 +3,12 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Why does the output print in the order it does?
  *
- * You answer:
+ * You answer: The parent must exist before the child. Therefore C_1's parent (B_1) must exist before C_1.
+ * And B_1's parent (A_1) must exist before B_2. C_1's constructor has an implicit call to super() which calls
+ * B_1's constructor.  B_1's constructor has an implicit super() call to A_1's constructor. A_1's constructor
+ * prints "Class A Constructor" and then returns execution flow to B_1's constructor which prints
+ * "Class B Constructor" and then returns flow of execution back to C_1's constructor which prints
+ * "Class A Constructor".
  *
  */
 
