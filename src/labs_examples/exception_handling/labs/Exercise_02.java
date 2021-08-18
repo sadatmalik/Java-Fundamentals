2fabf728b4cddf4748d226a042a0c3a45ee9a21f
sadat.malik <sadat.malik@me.com>
2021-08-18 12:53:41 +0100

    Completed exception_handling Exercie_02

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,38 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class TryCatchMultipleDemo {
+
+    public static void main(String[] args) {
+        // null pointer
+        System.out.println(divide(null));
+
+        // index out of bounds
+        System.out.println(divide(new int[] {1}));
+
+        // division by zero
+        System.out.println(divide(new int[] {3, 0}));
+
+        // ok
+        System.out.println(divide(new int[] {6, 3}));
+    }
+
+    public static int divide(int[] nums) {
+
+        int x = 0;
+
+        try {
+            x = nums[0] / nums[1];
+        } catch (ArrayIndexOutOfBoundsException aiex) {
+            aiex.printStackTrace();
+        } catch (ArithmeticException aex) {
+            aex.printStackTrace();
+        } catch (NullPointerException npe) {
+            npe.printStackTrace();
+        }
+        return x;
+    }
+
+}
+
 
