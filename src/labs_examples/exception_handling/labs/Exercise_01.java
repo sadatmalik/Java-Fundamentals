eea76b9607331d77ced5bd9a814bbe053b9915e5
sadat.malik <sadat.malik@me.com>
2021-08-18 10:37:19 +0100

    Complted exception_handling Exercise_01

--- src/labs_examples/exception_handling/labs/Exercise_01.java
+++ src/labs_examples/exception_handling/labs/Exercise_01.java
@@ -7,3 +7,23 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+class TryCatchDemo {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(division(6, 3));
+            System.out.println(division(3, 0));
+        } catch (ArithmeticException ae) {
+            System.out.println("Caught division by Zero attempt");
+        }
+    }
+
+    public static int division(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a/b;
+    }
+
+
+}
