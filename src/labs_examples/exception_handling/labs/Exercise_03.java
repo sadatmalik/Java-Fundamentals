bd06ac17eb2c671c7c5d0ac8724ed204c41f7348
sadat.malik <sadat.malik@me.com>
2021-08-18 12:57:53 +0100

    Completed exception_handling Exercise_03

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,28 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+class TryCatchFinallyDemo {
+
+    public static void main(String[] args) {
+
+        // exception case
+        System.out.println(divide(4, 0));
+
+        // no exception case
+        System.out.println(divide(4,2));
+    }
+
+    public static int divide(int a, int b) {
+
+        int x = -1;
+        try {
+            x = a / b;
+        } catch (ArithmeticException aex) {
+            aex.printStackTrace();
+        } finally {
+            System.out.println("divide(" + a + ", " + b + ")");
+        }
+        return x;
+    }
+}
