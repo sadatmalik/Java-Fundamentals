5ecad5e47cbba19f81317783072f04038c02caae
sadat.malik <sadat.malik@me.com>
2021-08-18 16:13:17 +0100

    Completed exception_handling Exercise_05

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -13,6 +13,20 @@ class Example {
 
     public static void main(String[] args) {
 
+        try {
+            System.out.println(divide(6, 0));
+        } catch (ArithmeticException aex) {
+            aex.printStackTrace();
+        }
+
+    }
+
+    public static int divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+
+        return a / b;
     }
 
 }
