438923c9081144e7d0206d63d4d44a6782f288be
sadat.malik <sadat.malik@me.com>
2021-08-19 19:27:24 +0100

    Completed input_output Exercise_01

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,7 +14,37 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
+        BufferedInputStream in = null;
+        BufferedOutputStream out = null;
 
+        try {
+            in = new BufferedInputStream(new FileInputStream("src/labs_examples/input_output/files/char_data.txt"));
+            out = new BufferedOutputStream(new FileOutputStream("src/labs_examples/input_output/files/char_data3.txt"));
+            byte[] bytes = new byte[5];
+            int read;
+
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                out.close();
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            }
+        }
 
     }
 }
