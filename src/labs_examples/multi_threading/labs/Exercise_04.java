19051fb0c5bb4ccdc21eff606f4c6b7454782e2d
sadat.malik <sadat.malik@me.com>
2021-08-22 11:17:49 +0100

    Completed multi_threading Exercise_04

--- src/labs_examples/multi_threading/labs/Exercise_04.java
+++ src/labs_examples/multi_threading/labs/Exercise_04.java
@@ -6,3 +6,43 @@ package labs_examples.multi_threading.labs;
  *      Demonstrate the use of a synchronized block and a synchronized method - ensure that the synchronization is
  *      working as expected
  */
+
+class BankAccount {
+
+    int balance;
+
+    public BankAccount(int balance) {
+        System.out.println("Opening balance: " + balance);
+        this.balance = balance;
+    }
+
+    public synchronized void deposit(int deposit, String name) {
+        System.out.println("\nHello, " + name);
+        System.out.println(name + ", you are depositing £" + deposit);
+        balance += deposit;
+        System.out.println(name + ", your new balance is £" + balance);
+    }
+
+    public void withdraw(int withdraw, String name) {
+        synchronized(this) {
+            System.out.println("\nHello, " + name);
+            System.out.println(name + ", you are withdrawing £" + withdraw);
+            balance -= withdraw;
+            System.out.println(name + ", your new balance is £" + balance);
+        }
+    }
+
+}
+
+class JointBankAccountDemo {
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(200);
+        account.deposit(20, "John");
+        account.deposit(25, "Jane");
+        account.withdraw(10, "John");
+        account.deposit(5, "Jane");
+        account.deposit(15, "Jane");
+        account.withdraw(30, "John");
+    }
+}
