1fd37fe5704d2b88cb56bf28ce8c402003215e1a
sadat.malik <sadat.malik@me.com>
2021-08-22 11:08:08 +0100

    Completed multi_threading Exercise_03

--- src/labs_examples/multi_threading/labs/Exercise_03.java
+++ src/labs_examples/multi_threading/labs/Exercise_03.java
@@ -5,3 +5,28 @@ package labs_examples.multi_threading.labs;
  *
  *      In one of the previous exercises, demonstrate changing the priority of a thread
  */
+
+class MyThreadWithPriority extends Thread {
+
+    public MyThreadWithPriority(String name, int priority) {
+        setName(name);
+        setPriority(priority);
+        start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running in " + getName());
+    }
+
+}
+
+class Application3 {
+
+    public static void main(String[] args) {
+
+        new MyThreadWithPriority("MyThread1", Thread.MIN_PRIORITY);
+        new MyThreadWithPriority("MyThread2", Thread.NORM_PRIORITY);
+        new MyThreadWithPriority("MyThread3", Thread.MAX_PRIORITY);
+    }
+}
