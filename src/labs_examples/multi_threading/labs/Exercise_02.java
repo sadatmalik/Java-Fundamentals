0905fde446b7ed152c38e40084475ed4f4a937a1
sadat.malik <sadat.malik@me.com>
2021-08-22 11:02:33 +0100

    Completed multi_threading Exercise_02

--- src/labs_examples/multi_threading/labs/Exercise_02.java
+++ src/labs_examples/multi_threading/labs/Exercise_02.java
@@ -5,3 +5,27 @@ package labs_examples.multi_threading.labs;
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class MyThread extends Thread {
+
+    public MyThread(String name) {
+        setName(name);
+        start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running in " + getName());
+    }
+
+}
+
+class Application2 {
+
+    public static void main(String[] args) {
+
+        new MyThread("MyThread1");
+        new MyThread("MyThread2");
+        new MyThread("MyThread3");
+    }
+}
