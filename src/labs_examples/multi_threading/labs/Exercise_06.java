11a23c66feb3ac44d5cdb02b80231df0ababd690
sadat.malik <sadat.malik@me.com>
2021-08-22 21:22:41 +0100

    Completed multi_threading Exercise_06

--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -6,3 +6,42 @@ package labs_examples.multi_threading.labs;
  *      Write a program that will print 1-100 sequentially from at least two distinct threads. For instance, thread1 will
  *      print "1", then thread2 will print "2", then thread1 will print "3", then thread2 will print "4" and so on.
  */
+
+class Counter {
+
+    int counter;
+
+    public synchronized int getCounter() {
+        return ++counter;
+    }
+}
+
+class PrintCount {
+
+    public static void main(String[] args) {
+
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(new CounterThread(counter), "t1");
+        Thread t2 = new Thread(new CounterThread(counter), "t2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class CounterThread implements Runnable {
+
+    Counter counter;
+
+    public CounterThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println(Thread.currentThread().getName() + ": " + counter.getCounter());
+        }
+    }
+}
