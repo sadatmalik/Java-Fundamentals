eb06ab225b6fa3807075190a56dbf5149ac01256
sadat.malik <sadat.malik@me.com>
2021-08-22 10:56:34 +0100

    Completed multi_threading Exercise_01

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -8,4 +8,25 @@ package labs_examples.multi_threading.labs;
  *
  */
 
+class MyRunnable implements Runnable {
 
+    @Override
+    public void run() {
+        System.out.println("I am running in - " + Thread.currentThread().getName());
+    }
+}
+
+class Application {
+
+    public static void main(String[] args) {
+
+        // Can initiate like this:
+        MyRunnable runnable = new MyRunnable();
+        Thread thread1 = new Thread(runnable, "Runnable1");
+        thread1.start();
+
+        // Or another way, as don't need to keep any references
+        new Thread(new MyRunnable(), "Runnable2").start();
+
+    }
+}
