d2fbe85a2d81781a972089a4cb3457ad8004a4f6
sadat.malik <sadat.malik@me.com>
2021-08-23 13:40:33 +0100

    Completed generics Exercise_01

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,44 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+class GenericClassDemo {
+
+    public static void main(String[] args) {
+        GenericClass<String, Integer> gen = new GenericClass<>("Sadat", 3);
+        System.out.println(gen.getT() + " " + gen.getS());
+
+        GenericClass<Double, Boolean> gen2 = new GenericClass<>(3.4, false);
+        System.out.println(gen2.getT() + " " + gen2.getS());
+
+        gen.setT("John");
+        System.out.println(gen.getT() + " " + gen.getS());
+
+    }
+}
+
+class GenericClass<T, S> {
+
+    T t;
+    S s;
+
+    public GenericClass(T t, S s) {
+        this.t = t;
+        this.s = s;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public S getS() {
+        return s;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    public void setS(S s) {
+        this.s = s;
+    }
+}
