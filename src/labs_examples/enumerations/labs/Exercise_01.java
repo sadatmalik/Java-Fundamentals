74b0cfe1aadd079390ea5a38752955ad50e24557
sadat.malik <sadat.malik@me.com>
2021-08-24 11:10:02 +0100

    src/labs_examples/enumerations/labs/Exercise_01.java

--- src/labs_examples/enumerations/labs/Exercise_01.java
+++ src/labs_examples/enumerations/labs/Exercise_01.java
@@ -8,3 +8,39 @@ package labs_examples.enumerations.labs;
  *      of this enum from a seperate class.
  */
 
+enum Month {
+    JANUARY("Still pretty dark out"),
+    FEBRUARY("Shortest month of the year"),
+    MARCH("Spring is here"),
+    APRIL("Uh oh, April showers"),
+    MAY("Happy birthday!"),
+    JUNE("The sun has got his hat on!"),
+    JULY("Longest day of the year"),
+    AUGUST("Summer starts to slow"),
+    SEPTEMBER("Getting cold again now"),
+    OCTOBER("Cold and wet now"),
+    NOVEMBER("A child is born!"),
+    DECEMBER("Merry Christmas!");
+
+    private String info;
+
+    private Month(String info) {
+        this.info = info;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+}
+
+class Controller {
+
+    public static void main(String[] args) {
+        Month month = Month.JANUARY;
+        System.out.println(month + "\n");
+
+        for (Month m : Month.values()) {
+            System.out.println(m + ": " + m.getInfo());
+        }
+    }
+}
