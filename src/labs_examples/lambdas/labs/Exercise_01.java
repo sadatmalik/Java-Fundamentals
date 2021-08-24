70d43349881c26ad38500fa958193df05dcd01e0
sadat.malik <sadat.malik@me.com>
2021-08-24 21:08:10 +0100

    Part completed lambdas Exercise_01

--- src/labs_examples/lambdas/labs/Exercise_01.java
+++ src/labs_examples/lambdas/labs/Exercise_01.java
@@ -6,12 +6,12 @@ package labs_examples.lambdas.labs;
  *      1) Demonstrate creating a functional interface with an abstract method that takes no parameters and returns void
  *      2) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
  *          an anonymous inner class from this interface.
- *
+ * @TODO
  *      3) Demonstrate creating a functional interface with an abstract method that takes 1 parameter and returns a
  *          value of the same type as the parameter
  *      4) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
  *          an anonymous inner class from this interface.
- *
+ *          
  *      5) Demonstrate creating a functional interface with an abstract method that takes 2 parameters and returns a
  *          value
  *      6) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
@@ -23,3 +23,25 @@ package labs_examples.lambdas.labs;
  *
  *
  */
+
+@FunctionalInterface
+interface FuncInterface {
+    void method();
+}
+
+class FuncInterfaceDemo {
+    public static void main(String[] args) {
+        FuncInterface lambda = () -> {
+            System.out.println("Hello from lambda");
+        };
+        lambda.method();
+
+        FuncInterface anonClass = new FuncInterface() {
+            @Override
+            public void method() {
+                System.out.println("Hello from anon inner class");
+            }
+        };
+        anonClass.method();
+    }
+}
