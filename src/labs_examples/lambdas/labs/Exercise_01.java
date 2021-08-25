d010ffadb381f5d353ebacd1faaf54e2a7594088
sadat.malik <sadat.malik@me.com>
2021-08-25 12:31:59 +0100

    Completed second part of lambdas Exercise_01

--- src/labs_examples/lambdas/labs/Exercise_01.java
+++ src/labs_examples/lambdas/labs/Exercise_01.java
@@ -6,12 +6,12 @@ package labs_examples.lambdas.labs;
  *      1) Demonstrate creating a functional interface with an abstract method that takes no parameters and returns void
  *      2) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
  *          an anonymous inner class from this interface.
- * @TODO
+ *
  *      3) Demonstrate creating a functional interface with an abstract method that takes 1 parameter and returns a
  *          value of the same type as the parameter
  *      4) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
  *          an anonymous inner class from this interface.
- *          
+ * @TODO
  *      5) Demonstrate creating a functional interface with an abstract method that takes 2 parameters and returns a
  *          value
  *      6) Implement the previous functional interface with a lambda expression and use it. Also demonstrate creating
@@ -29,8 +29,15 @@ interface FuncInterface {
     void method();
 }
 
+@FunctionalInterface
+interface FuncInterfaceSingleParam<T> {
+    public T method(T s);
+}
+
 class FuncInterfaceDemo {
     public static void main(String[] args) {
+
+        // No params functional interface
         FuncInterface lambda = () -> {
             System.out.println("Hello from lambda");
         };
@@ -43,5 +50,20 @@ class FuncInterfaceDemo {
             }
         };
         anonClass.method();
+
+
+        // Single param functional interface
+        FuncInterfaceSingleParam<String> lambda2 = (s) -> {
+            return s;
+        };
+        lambda2.method("Sadat");
+
+        FuncInterfaceSingleParam<String> anon2 = new FuncInterfaceSingleParam<>() {
+            @Override
+            public String method(String s) {
+                return s;
+            }
+        };
+        anon2.method("Sadat again");
     }
 }
