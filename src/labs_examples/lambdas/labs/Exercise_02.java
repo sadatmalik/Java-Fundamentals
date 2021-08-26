459f294f92debfedd8a6d78fad1e7b473454a58d
sadat.malik <sadat.malik@me.com>
2021-08-26 16:02:28 +0100

    Demo of java.util.function.Function as part 1/10 of lambdas Exercise_02

--- src/labs_examples/lambdas/labs/Exercise_02.java
+++ src/labs_examples/lambdas/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.lambdas.labs;
 
+import java.util.function.Function;
+
 /**
  * Lambdas Exercise 2:
  *
@@ -7,3 +9,22 @@ package labs_examples.lambdas.labs;
  *      10 of them! You can do it! You'll know so much about them when you're done!
  *
  */
+
+class JavaUtilFunctionsDemo {
+
+    public static void main(String[] args) {
+
+        Function<String, Integer> funcStringToInt = (s) -> Integer.parseInt(s);
+        Function<Integer, Integer> funcTimesTwo = (i) -> i * 2;
+        Function<Integer, String> funcIntToString = (i) -> String.valueOf(i);
+        Function<String, String> funcToUpperCase = (s) -> s.toUpperCase();
+
+        int i = funcTimesTwo.compose(funcStringToInt).apply("1");
+        System.out.println(i);
+
+        String s = funcIntToString.andThen(funcToUpperCase).apply(4);
+        System.out.println(s);
+    }
+
+
+}
