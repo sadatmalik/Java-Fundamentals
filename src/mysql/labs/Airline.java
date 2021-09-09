cdf4afa27c7639dd0333d333757cbb065008178a
sadat.malik <sadat.malik@me.com>
2021-09-09 09:38:38 +0100

    Populated Airline enum

--- src/mysql/labs/Airline.java
+++ src/mysql/labs/Airline.java
@@ -1,4 +1,21 @@
 package mysql.labs;
 
 public enum Airline {
+    BRITISH_AIRWAYS(1),
+    SINGAPORE_AIRLINES(2),
+    AMERICAN_AIRLINES(3),
+    CATHAY_PACIFIC(4),
+    JAPAN_AIRWAYS(5),
+    VIRGIN(6),
+    EMIRATES(7);
+
+    int airlineId;
+
+    private Airline(int id) {
+        airlineId = id;
+    }
+
+    public int getAirlineId() {
+        return airlineId;
+    }
 }
