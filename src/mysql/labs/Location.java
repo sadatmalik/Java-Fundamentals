dc56370a0fcd5bd72d1e1f52401c003331f752df
sadat.malik <sadat.malik@me.com>
2021-09-13 09:09:43 +0100

    Functionality to support Location enum mapping and lookup by Location ID

--- src/mysql/labs/Location.java
+++ src/mysql/labs/Location.java
@@ -1,5 +1,8 @@
 package mysql.labs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //@TODO: Can all these kinds of enums have their id details loaded in from the DB on initialisation??
 public enum Location {
     LONDON(1),
@@ -14,11 +17,24 @@ public enum Location {
 
     private final int locationId;
 
+    private static final Map<Integer, Location> locationsById;
+    
+    static {
+        locationsById = new HashMap();
+        for (Location loc : Location.values()) {
+            locationsById.put(loc.locationId, loc);
+        }
+    }
+
     private Location(int id) {
         locationId = id;
     }
 
     public int getLocationId() {
         return locationId;
+    }
+
+    public Location from(int id) {
+        return locationsById.get(id);
     }
 }
