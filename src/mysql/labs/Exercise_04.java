ec48f72c6940a06d94aa5b65dd1e97414e33150d
sadat.malik <sadat.malik@me.com>
2021-09-14 10:31:23 +0100

    Refactored createFlight method overloading

--- src/mysql/labs/Exercise_04.java
+++ src/mysql/labs/Exercise_04.java
@@ -37,13 +37,13 @@ public class Exercise_04 {
 
         Flight flight = new Flight(Plane.BOEING_747, Airline.AMERICAN_AIRLINES, "AA203",
                 Location.NEW_YORK, Location.DUBAI,
-                new Date(121, 8, 18, 16, 30),
-                new Date(121, 8, 18, 22, 00));
+                new Date(121, 8, 19, 16, 30),
+                new Date(121, 8, 19, 22, 00));
 
 
         /* Using method overloading to create flights with different parameters */
 
-        // createFlight(flight);
+        createFlight(flight);
         // createFlight(3, 1, "BA009", 1, Timestamp.valueOf("2021-09-08 15:00:00"), 8, Timestamp.valueOf("2021-09-08 23:00:00"));
 
 
@@ -119,6 +119,17 @@ public class Exercise_04 {
         }
     }
 
+    private static void createFlight(Flight flight) {
+
+        createFlight(flight.getPlaneType().getId(),
+                flight.getAirline().getAirlineId(),
+                flight.getFlightNum(),
+                flight.getSource().getLocationId(),
+                new Timestamp(flight.getDepartureDateTime().getTime()),
+                flight.getDestination().getLocationId(),
+                new Timestamp(flight.getArrivalDateTime().getTime()));
+
+    }
 
     private static void createFlight(int planeId, int airlineId, String flightNum,
                                      int source, Timestamp departure, int destination,
@@ -156,36 +167,6 @@ public class Exercise_04 {
                     ", departureDateTime=" + departure +
                     ", arrivalDateTime=" + arrival +
                     '}');
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void createFlight(Flight flight) {
-
-        String sql = "INSERT INTO flights " +
-                "(plane_id, airline_id, flight_num, source, " +
-                "departure_time, destination, arrival_time) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?);";
-
-        try {
-            PreparedStatement ps = dbConnection.prepareStatement(sql);
-
-            ps.setInt(1, flight.getPlaneType().getId());
-            ps.setInt(2, flight.getAirline().getAirlineId());
-            ps.setString(3, flight.getFlightNum());
-            ps.setInt(4, flight.getSource().getLocationId());
-            ps.setTimestamp(5, new Timestamp(flight.getDepartureDateTime().getTime()));
-            ps.setInt(6, flight.getDestination().getLocationId());
-            ps.setTimestamp(7, new Timestamp(flight.getArrivalDateTime().getTime()));
-
-            System.out.println(ps);
-
-            ps.executeUpdate();
-            ps.close();
-
-            System.out.println("Inserted Flight into DB: " + flight.toString());
 
         } catch (SQLException e) {
             e.printStackTrace();
