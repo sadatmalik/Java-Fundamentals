5e5fdd9141fedfed672e5a709af7c876160002ca
sadat.malik <sadat.malik@me.com>
2021-09-14 14:48:55 +0100

    Created Stack class for stack.labs Exercise_02

--- src/labs_examples/datastructures/stack/labs/Exercise_02.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_02.java
@@ -8,7 +8,7 @@ package labs_examples.datastructures.stack.labs;
  *
  *      Your custom Stack class must also do the following: (Ryan Take 2)
  *
- *      1) throw a custom exception when trying to pop an element from an empty Stack
+ *      done - 1) throw a custom exception when trying to pop an element from an empty Stack
  *      2) resize the Stack (the underlying array) to be twice the size when the Stack is more than 3/4 full
  *      3) resize the Stack (the underlying array) to be half the size when the Stack is more than 1/4 empty
  *      4) contain the methods peekFirst() and peekLast()
@@ -18,3 +18,105 @@ package labs_examples.datastructures.stack.labs;
  *      TIP: To initialize an new array of a Generic type you can use this:
  *           T[] data = (T[]) new Object[10];
  */
+
+class Stack<T> {
+
+    private T[] data;
+    private int pointer = 0;
+
+    public Stack() {
+        data = (T[]) new Object[10];
+    }
+
+    public void push(T item) {
+        data[pointer] = item;
+        pointer++;
+    }
+
+    public T peek() {
+        return data[pointer-1];
+    }
+
+    public T pop() throws StackUnderFlowException{
+        if (pointer == 0) {
+            throw new StackUnderFlowException("Stack empty");
+        }
+        T item = data[pointer-1];
+        data[pointer-1] = null;
+        pointer--;
+        return item;
+    }
+
+    public boolean empty() {
+        return pointer == 0;
+    }
+
+    public int search(T item) {
+        int depth = 0;
+        for (int i = pointer; i >= 0; i--) {
+            if (data[i] == item) {
+                return depth;
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
+
+class StackUnderFlowException extends Exception {
+
+    String msg;
+
+    public StackUnderFlowException(String message) {
+        this.msg = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return msg;
+    }
+
+}
+
+public class Exercise_02 {
+    public static void main(String[] args) {
+        Stack<String> myStack = new Stack<>();
+
+        // push
+        myStack.push("four");
+        myStack.push("three");
+        myStack.push("two");
+        myStack.push("one");
+
+        // peek
+        System.out.println(myStack.peek());
+
+        // pop
+        try {
+            System.out.println(myStack.pop());
+        } catch (StackUnderFlowException sufe) {
+            sufe.printStackTrace();
+        }
+
+        // search
+        System.out.println("Depth of \'four\' = " + myStack.search("four"));
+
+        // empty
+        while (!myStack.empty()) {
+            try {
+                System.out.println(myStack.pop());
+            } catch (StackUnderFlowException sufe) {
+                sufe.printStackTrace();
+            }
+        }
+
+        // uncomment to throw StackUnderflowException
+//        try {
+//            myStack.pop();
+//        } catch (StackUnderFlowException sufe) {
+//            System.out.println("Stack is empty -- cannot pop()");
+//            sufe.printStackTrace();
+//        }
+    }
+
+}
