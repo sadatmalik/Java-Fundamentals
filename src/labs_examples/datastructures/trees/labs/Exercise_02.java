7f38c28b5088768e42cebb666931abcaf4eb88ec
sadat.malik <sadat.malik@me.com>
2021-09-29 00:17:00 +0100

    Created custom Trie with insert function as first part of tree.labs Exercise_02

--- src/labs_examples/datastructures/trees/labs/Exercise_02.java
+++ src/labs_examples/datastructures/trees/labs/Exercise_02.java
@@ -1,5 +1,8 @@
 package labs_examples.datastructures.trees.labs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
     Trees: Exercise_02
 
@@ -15,3 +18,46 @@ package labs_examples.datastructures.trees.labs;
     Here is a fantastic set of relevant interview questions: https://www.techiedelight.com/trie-interview-questions/
 
  */
+class Trie {
+    Node root;
+
+    Trie() {
+        root = new Node();
+    }
+
+    // insert string
+    void insert(String s) {
+        insert(root, s);
+    }
+
+    void insert(Node node, String s) {
+        if (s.equals("")) {
+            node.endOfWord = true;
+            return;
+        } else {
+            if (!node.children.containsKey(s.charAt(0))) {
+                node.children.put(s.charAt(0), new Node());
+            }
+            insert(node.children.get(s.charAt(0)), s.substring(1, s.length()));
+        }
+    }
+
+}
+
+class Node {
+    Map<Character, Node> children;
+    boolean endOfWord;
+
+    Node() {
+        children = new HashMap<>();
+        endOfWord = false;
+    }
+
+}
+
+class Exercise_02 {
+    public static void main(String[] args) {
+        Trie t = new Trie();
+        t.insert("Sadat");
+    }
+}
