8c6d6670ea858e29909da304c676844ddbf4124b
sadat.malik <sadat.malik@me.com>
2022-03-24 09:29:28 +0000

    selection sort

---
+++ src/labs_examples/sorting_algorithms/labs/SelectionSort.java
@@ -0,0 +1,73 @@
+package labs_examples.sorting_algorithms.labs;
+
+import java.util.Arrays;
+
+/**
+ * The Selection Sort is another quadratic sorting algorithm (O(n2)).
+ *
+ * Like Bubble Sort and Insertion Sort, Selection Sort maintains the idea
+ * of having a "sorted" portion of the list, and an "unsorted" portion of
+ * the list which we build as we iterate through the list.
+ *
+ * The Selection Sort repeatedly finds the lowest/smallest element in the
+ * list, and moves it to the end of the "sorted" portion of the list. For
+ * example, let's assume we have this list of numbers:
+ *
+ * 34, 24, 8, 11, 7
+ *
+ * On the first pass of the loop, the Selection Sort will identify 7 as the
+ * lowest number, and it will swap 7 & 34. Then it will consider 7 as the
+ * beginning of the "sorted" portion of the list.
+ *
+ * 7   |   24, 8, 11, 34
+ *
+ * On the second pass, it will identify 8 as the lowest number and swap it
+ * with 24. Then it will move the "sorted" indicator one to the right.
+ *
+ * 7, 8   |   24, 11, 34
+ * On the third pass, it will identify 11 as the lowest number and swap it
+ * with 24. Then it will move the "sorted" indicator one to the right.
+ *
+ * 7, 8, 11  |   24, 34
+ *
+ * On the fourth pass, it will identify 24 as the lowest number but there is
+ * no need to swap as it's already in position. All we need to do is move the
+ * "sorted" indicator one to the right.
+ *
+ * 7, 8, 11, 24 | 34
+ *
+ * On the fifth pass, it will identify 34 as the only (unsorted) number
+ * remaining, so we can assume the list is sorted.
+ *
+ * 7, 8, 11, 24, 34
+ *
+ * @author sm@creativefusion.net
+ */
+public class SelectionSort {
+
+    public static void selectionSort(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int minPos = i;
+
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] < nums[minPos]) {
+                    minPos = j;
+                }
+            }
+
+            if (minPos != i) {
+                int tmp = nums[i];
+                nums[i] = nums[minPos];
+                nums[minPos] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {34, 24, 8, 11, 7};
+        selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
