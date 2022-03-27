3ac985ade30802d56cae6d1eadde09d6665c1852
sadat.malik <sadat.malik@me.com>
2022-03-27 17:24:15 +0100

    merge sort - description

---
+++ src/labs_examples/sorting_algorithms/labs/MergeSort.java
@@ -0,0 +1,19 @@
+package labs_examples.sorting_algorithms.labs;
+
+/**
+ * The Merge sort is a divide and conquer algorithm that was invented by John
+ * von Neumann in 1945. A detailed description and analysis of bottom-up mergesort
+ * appeared in a report by Goldstine and von Neumann as early as 1948.
+ *
+ * Conceptually, a merge sort works as follows:
+ *
+ * 1. Divide the unsorted list into n sub-lists, each containing one element (a list
+ * of one element is considered sorted).
+ *
+ * 2. Repeatedly merge sub-lists to produce new sorted sub-lists until there is only
+ * one sub-list remaining. This will be the sorted list.
+ *
+ * @author sm@creativefusion.net
+ */
+public class MergeSort {
+}
